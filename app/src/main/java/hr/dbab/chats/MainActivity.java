3c5234a54ae9f9b5b500ec217c7502af46fc4a6f
babic <babdan5@gmail.com>
2019-12-12 23:55:29 +0100

    Created layout for the MainActivity and added a toolbar

--- app/src/main/java/hr/dbab/chats/MainActivity.java
+++ app/src/main/java/hr/dbab/chats/MainActivity.java
@@ -1,14 +1,23 @@
 package hr.dbab.chats;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar mToolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Instantiating the toolbar and setting it as ActionBar
+        mToolbar = findViewById(R.id.toolbar_main);
+        setSupportActionBar(mToolbar);
+        // Set the title of the toolbar
+        getSupportActionBar().setTitle("Chats");
     }
 }
